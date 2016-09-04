b787bbf628db53812b47e5ab9a134a6c84a3b2fb
Vortex <c.spielvogel@ymail.com>
2016-09-04 18:04:13 +0200

    auto commit

---
+++ while.sh
@@ -0,0 +1,11 @@
+LOOP=0
+while [[ ${LOOP} != "Everything\ up-to-date" ]]
+        do
+                git pull
+		git add --all
+		git commit -m "auto commit"
+		git push
+		LOOP="$(git push)"
+		echo "${LOOP}"
+        done
+
