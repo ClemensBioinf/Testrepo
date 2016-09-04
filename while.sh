860aa1fd9260d834dfc1833b7d4ba7b555575b72
Vortex <c.spielvogel@ymail.com>
2016-09-04 18:50:02 +0200

    auto commit

--- while.sh
+++ while.sh
@@ -1,12 +1,13 @@
-LOOP="True"
-while [[ ${LOOP} != "*Everything\ up-to-date*" ]]
-        do
-                git pull
-		git add --all
-		git commit -m "auto commit"
-		git push
-		LOOP="$(git push)"
-		echo "${LOOP}"
-		/bin/sleep 2
-        done
-
+OUTPUT="None"
+while true; do
+	git pull
+	git add --all
+	git commit -m "auto commit"
+	git push
+	OUTPUT="$(git push)"
+	/bin/sleep 2
+        if [[ $OUTPUT =~ "Everything\ up-to-date" ]]; then
+		echo "Pushing successful."
+		break
+	fi
+done
